b0f69999bb856a6797dbad150bb3a8326ddb67f6
NoistNT <arielgnr23@gmail.com>
2024-03-14 11:51:02 -0300

    Add ESLint and lint files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [{ hostname: 'rickandmortyapi.com' }, { hostname: 'localhost' }, { hostname: 'example.com' }],
+    loader: 'default'
+  }
+};
 
 export default nextConfig;
