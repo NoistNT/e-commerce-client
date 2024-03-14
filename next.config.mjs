b8481eadb26e22b2ebf99b934b0eccce4c914761
NoistNT <arielgnr23@gmail.com>
2024-03-14 19:52:12 -0300

    Add shadcn

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [{ hostname: 'rickandmortyapi.com' }, { hostname: 'localhost' }, { hostname: 'example.com' }],
+    remotePatterns: [{ hostname: 'res.cloudinary.com' }],
     loader: 'default'
   }
 };
